a5af34da527c90a034a71751f593f7cab150033c
winiseptiani654-del <winiseptiani654@gmail.com>
2026-01-22 12:59:48 +0700

    UAS

--- Latihan6.py
+++ Latihan6.py
@@ -1,8 +1,8 @@
 # Definisikan fungsi untuk menyatakan perasaan
-def (perasaan):
-    if  == "senang":
+def kondisi (perasaan):
+    if kondisi  == "senang":
         return "Saya jika ini UAS Dapat A!"
-    elif  == "sedih":
+    elif kondisi == "sedih":
         return "Saya saya sedih karena mengerjakan menggunakan ChatGPT."
     else:
         return "saya akan mengulang perkuliahan ini dengan baik."
